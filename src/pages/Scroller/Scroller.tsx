f3fa97164ff97e7cc0079a9ccd08c48167f5f6cb
Petro Davybida <petdavy1991@gmail.com>
2022-12-30 01:19:19 +0200

    fix re-render in expensiveComponent on scroll

--- src/pages/Scroller/Scroller.tsx
+++ src/pages/Scroller/Scroller.tsx
@@ -1,14 +1,13 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, memo } from 'react';
 import { CenteredLayout } from '~/components';
-
-// TODO how can we optimize, prevent re-rendering ExpensiveComponent
-// by changing component structure ?
 
 const ExpensiveComponent = () => {
   const now = performance.now();
   while (performance.now() - now < 100) {}
-  return <div>Ohh.. so expensive</div>;
+  return <div>Hmm.. not so bad</div>;
 };
+
+const MemoizedExpensiveComponent = memo(ExpensiveComponent);
 
 export const Scroller = () => {
   const [scrollTop, setScrollTop] = useState(0);
@@ -29,7 +28,7 @@ export const Scroller = () => {
       <CenteredLayout className="gap-4 fixed top-0 left-1/2 -translate-x-1/2">
         <div className="text-3xl">See the code</div>
         <div>{scrollTop} px</div>
-        <ExpensiveComponent />
+        <MemoizedExpensiveComponent />
       </CenteredLayout>
     </div>
   );

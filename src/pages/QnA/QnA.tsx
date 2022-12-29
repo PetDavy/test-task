5160d61344744ba041ba544826b8bcbf7d3ae09a
Petro Davybida <petdavy1991@gmail.com>
2022-12-29 23:50:26 +0200

    Refactor QnA (refactor-2)

--- src/pages/QnA/QnA.tsx
+++ src/pages/QnA/QnA.tsx
@@ -1,35 +1,27 @@
 import { CenteredLayout } from '~/components';
+import untypedQnAItems from './items.json';
 
-// TODO refactor
+const QnAList = untypedQnAItems as QnAItem[];
 
-interface QuestionOrAnswer {
-  question?: string;
-  answer?: string;
+interface QnAItem {
+  id: number;
+  question: string;
+  answer: string;
 }
 
-const QnAList: QuestionOrAnswer[] = [
-  { question: 'Do you run like a fish?' },
-  { answer: 'Absolutely man' },
-  { question: 'Have you tried to swim like a dinosaur?' },
-  { answer: 'Nah, not my cup of tea' },
-  { question: 'How are we counting from 5 to 10?' },
-  { answer: 'Do I look like a counter?' },
-];
-
-const QnaRender = ({ question, answer }: QuestionOrAnswer) => {
-  if (question) {
-    return <h3 className="font-bold text-lg">{question}</h3>;
-  } else {
-    return <p className="mb-2">{answer}</p>;
-  }
-};
+const QnaRender = ({ question, answer }: QnAItem) => (
+  <>
+    <h3 className="font-bold text-lg">{question}</h3>
+    <p className="mb-2">{answer}</p>
+  </>
+);
 
 export const QnA = () => {
   return (
     <CenteredLayout className="gap-2">
       <div className="text-3xl mb-2">See the code</div>
-      {QnAList.map((item, index) => (
-        <QnaRender key={index} {...item} />
+      {QnAList.map((item) => (
+        <QnaRender key={item.id} {...item} />
       ))}
     </CenteredLayout>
   );
